c02f737cbbb0a321fad5782a8fbd92a34c5f1e94
Janakiram <89788712+janakiram180@users.noreply.github.com>
2024-07-11 12:05:47 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,35 +1,48 @@
-from flask import Flask,render_template,request,jsonify
+from flask import Flask, render_template, request, jsonify
 import json
-from flask_cors import CORS
+import chatgpt
 app = Flask(__name__)
-CORS(app)
 
-
+# the home route
 @app.route('/')
 def welcome():
-          
-        # print(data['body']) 
-        return render_template('index.html')
+    return render_template('index.html')
 
-@app.route("/reciver",methods = ['GET','POST'])
-def reciver():
-        txt ='o'
-        if request.method == 'POST':
-          data = request.json
-          txt= data['data']
-          print(txt)
-        
-        elif request.method == 'GET':
-                     ex = {
-                          'data': "hello world"
-                     }
-                     jsonStr = json.dumps(ex)
-                     return jsonify(Response=jsonStr)
-        #  print(request.json)
 
-        return txt
-                 
-        
+x = ''
+
+
+async def text(txt):
+    global x
+    x = txt
+    return x
+
+
+async def anr():
+    global x
+    return chatgpt.answer(str(x))
+
+
+# THE ROUTE FOR THE GET REQUEST
+@app.route('/answer', methods=['GET'])
+async def answer():
+    if request.method == 'GET':
+        ex = {
+            'data': await anr()
+        }
+        jsonStr = json.dumps(ex)
+        return jsonify(Response=jsonStr)
+
+
+
+
+# The route for post request
+@app.route("/reciver", methods=['POST'])
+async def reciver():
+    data = request.json
+    txt = data['data']
+    print(await text(txt))
+    return ""
 
 
 if __name__ == '__main__':
